b8bf7d8c4b88d16dd1e409c9416deb2c010de066
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-06-25 16:20:33 +0000

    connect frontend to external IP

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af7290cdcc7384af6a43c995d0833d56-1706107907.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
